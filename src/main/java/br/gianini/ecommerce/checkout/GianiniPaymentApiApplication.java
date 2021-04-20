4a777129442322c0af6dbd88a53684d04048ae29
Fernando Vinícius Gianini Silva <gianini@angularlabs.com.br>
2021-04-19 23:30:18 -0300

    first commit

---
+++ src/main/java/br/gianini/ecommerce/checkout/GianiniPaymentApiApplication.java
@@ -0,0 +1,13 @@
+package br.gianini.ecommerce.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GianiniPaymentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GianiniPaymentApiApplication.class, args);
+	}
+
+}
